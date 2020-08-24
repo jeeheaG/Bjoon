9075fe80c7e0587723339c9318446efc76c60f36
unknown <jeehea99@naver.com>
2020-08-25 01:08:04 +0900

    adding files

---
+++ 백준 Java/Step1/src/Pro3.java
@@ -0,0 +1,9 @@
+
+public class Pro3 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
