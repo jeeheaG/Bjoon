98464e2ae4e39d92c3e1ae66daf5e9c50bbaa06b
unknown <jeehea99@naver.com>
2020-09-22 22:33:53 +0900

    0922 파이썬 pro7_2번 완성.품.

--- 백준 Python/Step7 함수/pro7_2.py
+++ 백준 Python/Step7 함수/pro7_2.py
@@ -1,18 +1,19 @@
 #답은 맞는 거 같은데 백준 채점 시간초과 뜸.. 내일 다시!!!!
+big = 10001
+num = list(range(big))
 
-num = list(range(10000))
-
-for n in range(10000) :
-    while True :    #n이 초기생성자로 d(n) 무한 반복
-        gen = n + n%10 + n//10 + n//100 + n//1000 + n//10000
-        #print(n," 일 때 생성된 수는",gen)
-        n = gen     #d(n)의 반복을 위함.
-        if gen >10000 or gen == 0 :     #생성된 수가 10000을 넘으면 while문을 break하고 다음 생성자로
-            break
+for n in range(big) : #n이 초기생성자
+        gen = n + n%10 + n//10%10 + n//100%10 + n//1000%10 + n//10000
         try :
             num.remove(gen)
-        except :    #이미 삭제한 숫자라면 다음 d(n)반복으로.
+            ''' 오류를 캐냈던 코드
+            if gen == 9993 :
+                    print(gen,'을 지운 생성자는', n)
+                    print(n, '일 때 ', n//10000, n//1000%10, n//100%10, n//10%10, n%10, n, " 생성된 수는",gen)
+                '''
+        except :    #이미 삭제한 숫자라면 다음 n으로.
             continue
-        
+
+#print(num)
 for i in num :
     print(i)
