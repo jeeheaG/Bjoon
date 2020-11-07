233fa947937e72a58c2a02425517c976e09a98f4
unknown <jeehea99@naver.com>
2020-11-07 23:54:49 +0900

    1107 자바 pro7_6번 품

---
+++ 백준 Java/Step7_new 문자열/src/Pro7_6_no1152.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Pro7_6_no1152 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String ss = br.readLine().trim();
+		String s[] = ss.split(" ");
+		
+		System.out.println(s.length);
+//		for(int i=0; i<s.length; i++) {
+//			System.out.println(s[i]);
+//		}
+	}
+
+}
