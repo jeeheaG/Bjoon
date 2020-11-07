b8bd9898e85f0e978cccba2d7a61a4c4ecf3d597
unknown <jeehea99@naver.com>
2020-11-08 00:02:17 +0900

    1107 자바 pro7_6번 설명 추가

--- 백준 Java/Step7_new 문자열/src/Pro7_6_no1152.java
+++ 백준 Java/Step7_new 문자열/src/Pro7_6_no1152.java
@@ -2,10 +2,15 @@ import java.io.*;
 public class Pro7_6_no1152 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String ss = br.readLine().trim();
-		String s[] = ss.split(" ");
+		String s[] = br.readLine().split(" ");
+		int count = s.length;
+		for (int i=0; i<count; i++) {//띄어쓰기가 중간에 여러개 연속으로 있을수도 있으므로 trim()으로는 모두 제거할 수 없음. count를 따로 해서 공백인 문자열을 확인해 빼주어야 함.
+			if(s[i]=="") {
+				count-=1;
+			}
+		}
 		
-		System.out.println(s.length);
+		System.out.println(count);
 //		for(int i=0; i<s.length; i++) {
 //			System.out.println(s[i]);
 //		}
