f039ba948b0004ac309dde1cdc02fea203f9decd
unknown <jeehea99@naver.com>
2020-11-10 23:58:47 +0900

    1110 파이썬 pro7_8번 품

---
+++ 백준 Python/Step7 문자열 (단계번호 바뀜 후)/pro7_8 NO.5622.py
@@ -0,0 +1,31 @@
+s = input()
+t = 0
+for i in range(len(s)):
+    if s[i] in 'ABC':
+        t+=3
+        continue;
+    elif s[i] in 'DEF' :
+        t+=4
+        continue;
+    elif s[i] in 'GHI' :
+        t+=5
+        continue;
+    elif s[i] in 'JKL' :
+        t+=6
+        continue;
+    elif s[i] in 'MNO' :
+        t+=7
+        continue;
+    elif s[i] in 'PQRS' :
+        t+=8
+        continue;
+    elif s[i] in 'TUV' :
+        t+=9
+        continue;
+    elif s[i] in 'WXYZ' :
+        t+=10
+        continue;
+    elif s[i] in 'DEF' :
+        t+=11
+        continue;
+print(t)
